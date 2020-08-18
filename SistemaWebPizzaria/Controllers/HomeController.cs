23329dd68c7f5948a4bfaf01dca2cb8c36da3faa
Edson Loiola <edson.monteiro.l@gmail.com>
2020-08-18 20:26:53 -0300

    Inicio projeto

---
+++ SistemaWebPizzaria/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaWebPizzaria.Models;
+
+namespace SistemaWebPizzaria.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+
+        public IActionResult Login()
+        {
+            ViewData["Message"] = "Login aplication.";
+
+            return View();
+        }
+
+        public IActionResult Plano()
+        {
+            ViewData["Message"] = "Veja aqui os planos.";
+
+            return View();
+        }
+
+       
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
