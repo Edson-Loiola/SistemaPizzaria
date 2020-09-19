566005ed47b1e0c6360166723ee765d9fab16932
Jose wellison de Sousa <wellison_designer@hotmail.com>
2020-09-19 13:50:55 -0300

    commit teste do sinc

---
+++ SistemaWebPizzaria/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using SistemaWebPizzaria.Services;
+
+namespace SistemaWebPizzaria.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly LoginService _loginService;
+
+        public LoginController(LoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+
+       
+
+       
+    }
+}
