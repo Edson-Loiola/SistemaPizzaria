0bcb4fbbb0dfc38d6bafef41ef38876aa291c3f5
izaias-alexandre <36794076+izaias-alexandre@users.noreply.github.com>
2020-10-10 21:33:55 -0300

    alt no funcionario

--- SistemaWebPizzaria/Models/Despesa.cs
+++ SistemaWebPizzaria/Models/Despesa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemaWebPizzaria.Models
 {
@@ -8,6 +9,7 @@ namespace SistemaWebPizzaria.Models
         public int IdDespesa { get; set; }
         public string NomeDespesa { get; set; }
         public string Descricao { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public decimal Valor { get; set; }
         public DateTime? DataDespesa { get; set; }
     }
